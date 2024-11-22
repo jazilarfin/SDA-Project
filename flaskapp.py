8dd0375bae4ee31ce5475bc33e938e694ee8001c
jazilarfin <jazilarfin@gmail.com>
2024-11-22 17:49:39 +0500

    Added signin page

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 import os
